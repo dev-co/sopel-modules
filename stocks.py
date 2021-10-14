6b4e88b20fecdad692fa3772d01c1656e5271d03
Luis Uribe <acme@riseup.net>
2021-10-14 14:38:35 -0500

    Adding try to requests

--- stocks.py
+++ stocks.py
@@ -1,21 +1,21 @@
 import sopel
 import requests
 
-def symbol_lookup(symb):
-    url = "https://rest.yahoofinanceapi.com/v6/finance/quote"
-    querystring = {"symbols": symb}
-    headers = {'x-api-key': "FAKE_API_KEY"}
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    return response.json()['quoteResponse']['result'][0]
-
 @sopel.module.commands('dji','stocks','stock')
 def stocks(bot,trigger):
     if trigger.group(2):
-        name = symbol_lookup(trigger.group(2))
+        url = "https://rest.yahoofinanceapi.com/v6/finance/quote"
+        querystring = {"symbols": trigger.group(2)}
+        headers = {'x-api-key': "FAKE_API_KEY"}
+        try:
+            response = requests.request("GET", url, headers=headers, params=querystring)
+            stock = response.json()['quoteResponse']['result'][0]
+        except requests.exceptions.RequestException as e:
+            return bot.say("An error ocurred")
     else:
         return bot.say("Please use a valid symbol")
 
-    change = '{0:.2f}'.format(float(name['regularMarketChange']))
-    percent = '{0:.2f}'.format(float(name['regularMarketChangePercent']))
+    change = '{0:.2f}'.format(float(stock['regularMarketChange']))
+    percent = '{0:.2f}'.format(float(stock['regularMarketChangePercent']))
 
-    return bot.say("{0}: {1} ({2}/{3})".format(name['longName'], name['regularMarketPrice'],"\x0304"+change+"\x0F" if float(change) < 0 else "\x0303"+change+"\x0F","\x0304"+percent+"%\x0F" if float(percent) < 0 else "\x0303+"+percent+"%\x0F",str(name['regularMarketOpen'])))
+    return bot.say("{0}: {1} ({2}/{3})".format(stock['longName'], stock['regularMarketPrice'],"\x0304"+change+"\x0F" if float(change) < 0 else "\x0303"+change+"\x0F","\x0304"+percent+"%\x0F" if float(percent) < 0 else "\x0303+"+percent+"%\x0F",str(stock['regularMarketOpen'])))
